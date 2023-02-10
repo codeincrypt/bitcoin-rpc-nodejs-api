fb33c408cd9526868b670e7e282ed7c01af1584c
codeincrypt <kartikkr555@gmail.com>
2023-02-11 02:32:03 +0530

    added getaddressinfo api

--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,8 @@ require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const USER = process.env.RPC_USER;
 const PASS = process.env.RPC_PASSWORD;
 
-const ENDPOINT = '127.0.0.1:8332'
-const ENDPOINTURL = `http://${USER}:${PASS}@${ENDPOINT}`
+const BTCRPC = '127.0.0.1:8332'
+const ENDPOINTURL = `http://${USER}:${PASS}@${BTCRPC}`
 
 const headers = {
   "content-type": "text/plain;"
@@ -83,5 +83,28 @@ router.get("/gettransaction/:hash", (req, res) => {
   };
   request(options, callback);
 });
+
+
+router.get("/getaddressinfo/:address", (req, res) => {
+  var dataString = `{"jsonrpc": "1.0", "id": "paybito", "method": "getaddressinfo", "params": ["${req.params.address}"]}`;
+  var options = {
+    url: ENDPOINTURL,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.json({statuscode:1, data:data, error:null});
+    } else {
+      console.log('error', error)
+      return res.json({statuscode:0, data:null, error:error});
+    }
+  };
+  request(options, callback);
+});
+
 
 module.exports = router;
